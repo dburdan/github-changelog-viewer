920213ee91084491893b0933c8975a6ce1ced06f
Dacri Burdan <dburdan@gmail.com>
2021-05-25 08:47:25 -0700

    Update button CSS to match GitHub changes

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -13,7 +13,7 @@ const getNavItemHtml = (href: string, numChangelogs: number) => {
     <a
       id="${constants.DOM.CHANGELOG_NAV_DIV_ID}"
       href="${href}"
-      class="ml-3 link-gray-dark no-underline position-relative"
+      class="ml-3 Link--primary no-underline position-relative"
     >
       <svg text="gray" height="16" class="octicon octicon-tag text-gray" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true">
         <path fill-rule="evenodd" d="M2 4a1 1 0 100-2 1 1 0 000 2zm3.75-1.5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zm0 5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zm0 5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zM3 8a1 1 0 11-2 0 1 1 0 012 0zm-1 6a1 1 0 100-2 1 1 0 000 2z"></path>
